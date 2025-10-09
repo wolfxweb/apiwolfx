432d808f53dcbd45b58a5079e2694eb8345c04da
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2025-10-09 08:11:43 -0300

    4234

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,7 +9,7 @@ class Settings:
         # Mercado Livre API Configuration
         self.ml_app_id = os.getenv("ML_APP_ID", "6987936494418444")
         self.ml_client_secret = os.getenv("ML_CLIENT_SECRET", "puvG9Z7XBgICZg5yK3t0PAXAmnco18Tl")
-        self.ml_redirect_uri = "https://9db9c61e2760.ngrok-free.app/api/callback"
+        self.ml_redirect_uri = "https://bb32abe46f92.ngrok-free.app/api/callback"
         
         # API Configuration
         self.api_host = os.getenv("API_HOST", "0.0.0.0")
